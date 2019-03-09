c2c7da14c8836bf0db29170571247f0c0fb3d1b4
gaber85 <gabrielr85@gmail.com>
2019-03-10 02:48:23 +1300

    chore: format code with prettier

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
-import moment from 'moment';
-import LineGraph from '../components/LineGraph'
-import jsonData from '../weatherData/data.json';
+import React, { Component } from "react";
+import moment from "moment";
+import LineGraph from "../components/LineGraph";
+import jsonData from "../weatherData/data.json";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -15,34 +15,44 @@ class Dashboard extends Component {
     this.getSurfaceSeaWaterSpeed();
   }
 
-  async getSurfaceSeaWaterSpeed () {
+  async getSurfaceSeaWaterSpeed() {
     const surfaceSeaWaterSpeed = [];
-    Object.keys(jsonData).sort().forEach(key => {
-      if (key && jsonData[key].surface_sea_water_speed) {
-        surfaceSeaWaterSpeed.push({key,value:jsonData[key].surface_sea_water_speed})
-      }
-    });
+    Object.keys(jsonData)
+      .sort()
+      .forEach(key => {
+        if (key && jsonData[key].surface_sea_water_speed) {
+          surfaceSeaWaterSpeed.push({
+            key,
+            value: jsonData[key].surface_sea_water_speed
+          });
+        }
+      });
     await this.setState({
       data: {
-        labels: surfaceSeaWaterSpeed.map(entry => moment(entry.key).format('MMM D, h:mm:ss a')),
+        labels: surfaceSeaWaterSpeed.map(entry =>
+          moment(entry.key).format("MMM D, h:mm:ss a")
+        ),
         datasets: [
           {
-            label: 'Speed m/s',
-            data: surfaceSeaWaterSpeed.map(entry=> entry.value)
+            label: "Speed m/s",
+            data: surfaceSeaWaterSpeed.map(entry => entry.value)
           }
-        ],
-      } 
-    })
+        ]
+      }
+    });
   }
-  
+
   render() {
     const { data } = this.state;
     return (
       <div>
-        <LineGraph graphData={data} text="Surface Seawater Speed (Nov 7 - 14 2018)"/>
+        <LineGraph
+          graphData={data}
+          text="Surface Seawater Speed (Nov 7 - 14 2018)"
+        />
       </div>
-      );
-  } 
+    );
+  }
 }
 
 export default Dashboard;
